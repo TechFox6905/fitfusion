cc00f740f3cfaa85afd7757c6fab84b09a73c15e
Prathamesh <prathamesh.gharat07@gmail.com>
2025-06-28 22:13:38 +0530

    Fix some folder and files structure

--- backend/api/healthcheck.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/healthcheck', (req, res) => {
-  res.json({ message: 'API is working' });
-});
-
-module.exports = router;
